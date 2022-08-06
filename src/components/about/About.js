83214cf442fb8f08d7e6a2e313b6fb5182d3f0b0
Samuel <samuelgarenas@gmail.com>
2022-08-05 20:45:00 -0400

    issue fixed, now doing about me. next portfolio

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -13,10 +13,21 @@ const About = () => {
                 <div className="img2-cover">
                      <img src={Me2} alt= "" className="about-img"/>
                 </div>
-                
+
             </div>
 
          <div className="about-right">
+
+            <h1 className="about-title">About Me</h1>
+
+            <p className="about- desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Soluta perspiciatis officia atque alias! Delectus, nisi ducimus. Itaque, obcaecati 
+            officia sapiente architecto illo nostrum provident enim est tempore facilis ad repellendus.
+            </p>
+
+
+
          </div>
 
 
